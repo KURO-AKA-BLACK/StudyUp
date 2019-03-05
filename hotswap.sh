9590eb1e9ee5d131060b8316dc3ba8461f6b7e40
KURO-AKA-BLACK <zrycheng@ucdavis.edu>
2019-03-04 23:22:53 -0800

    hotswap.sh v2

--- hotswap.sh
+++ hotswap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ORIGIN= sed -n 11p /etc/nginx/nginx.conf
-if [ "$ORIGIN" != "    server $@:6379;" ]
+if grep -q "$@:6379" /etc/nginx/nginx.conf
 then
-    sed -i '' "11s/.*/    server $@:6379;/" /etc/nginx/nginx.conf
-    /usr/sbin/nginx -s reload
-else
     echo "The IP address is already being used."
+else
+    sed -i "11s/.*/    server $@:6379;/" /etc/nginx/nginx.conf
+    /usr/sbin/nginx -s reload
 fi
+/usr/sbin/nginx -s reload
